affd72a8874798ff8109a93b27dd72d532b2d27a
digitalbiz-tech <157323900+digitalbiz-tech@users.noreply.github.com>
2024-01-22 12:16:12 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read()
 
-df = df[df['Order Number'] != '']
+df = df[df['Order Number'] != 'None']
 st.write("Here's the list of the Orders made so far")
 edited_df = st.experimental_data_editor(df)
 
