6d29a82da4fed9cc6a17d215b0e5fafa17972e89
digitalbiz-tech <157323900+digitalbiz-tech@users.noreply.github.com>
2024-01-22 12:13:48 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read()
 
-
+df = df.dropna()
 st.write("Here's our first attempt at using data to create a table:")
 df2 = pd.DataFrame({'first column': [1, 2, 3, 4], 'second column': [10, 20, 30, 40]})
 edited_df = st.experimental_data_editor(df)
