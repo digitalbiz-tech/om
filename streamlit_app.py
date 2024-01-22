8c838201301e5caefc4b27fd2aef0f2b4f170227
digitalbiz-tech <157323900+digitalbiz-tech@users.noreply.github.com>
2024-01-22 12:39:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,30 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read()
 
-df = df[df['Order Number'] != NaN]
+df = df[df['Order Number'] != ""]
 st.write("Here's the list of the Orders made so far")
-edited_df = st.experimental_data_editor(df)
+edited_df = st.data_editor(
+    df,
+    column_config={
+        "Order Number": "Order ID",
+        "Order Status": st.column_config.NumberColumn(
+            "Your rating",
+            help="How much do you like this command (1-5)?",
+            min_value=1,
+            max_value=5,
+            step=1,
+            format="%d ⭐",
+        ),
+        "Order Date": "Ordered Date",
+      "OrderContact": "Ordered By",
+      "Order Type": "Order Type",
+      "Expected Order Date": "Expected Delivery date",
+      "Notes": "Notes",
+
+    },
+    disabled=["Notes"],
+    hide_index=True,
+)
 
 # Print results.
 #for row in df.itertuples():
