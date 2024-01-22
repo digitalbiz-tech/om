12c8d87340b99d0dd46e60f857fe657d4f31de91
digitalbiz-tech <157323900+digitalbiz-tech@users.noreply.github.com>
2024-01-22 12:11:52 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,11 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read()
 
+
 st.write("Here's our first attempt at using data to create a table:")
 df2 = pd.DataFrame({'first column': [1, 2, 3, 4], 'second column': [10, 20, 30, 40]})
-edited_df = st.experimental_data_editor(df2)
+edited_df = st.experimental_data_editor(df)
 
 # Print results.
-for row in df.itertuples():
-    st.write(f"{row.OrderContact}")
+#for row in df.itertuples():
+#    st.write(f"{row.OrderContact}")
