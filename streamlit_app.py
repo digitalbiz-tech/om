4f7e9e3c412453d3b661c167ca429ef31840dd26
digitalbiz-tech <157323900+digitalbiz-tech@users.noreply.github.com>
2024-01-22 12:15:48 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,8 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read()
 
-df = df.dropna()
-st.write("Here's our first attempt at using data to create a table:")
-df2 = pd.DataFrame({'first column': [1, 2, 3, 4], 'second column': [10, 20, 30, 40]})
+df = df[df['Order Number'] != '']
+st.write("Here's the list of the Orders made so far")
 edited_df = st.experimental_data_editor(df)
 
 # Print results.
